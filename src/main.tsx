11f4107c629937687f8570fb1329ff920f7b8076
MGliem <maximilian.gliem@gmail.com>
2024-11-26 16:00:42 +0100

    feat(style): body global css background and text color

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ChakraProvider, createSystem, defineConfig, defaultConfig } from "@chakra-ui/react"
 import App from "./App.tsx";
+
+const themeConfig = defineConfig({
+  globalCss: {
+    "body": {
+      background: "#262626",
+      color: "#cecece"
+    }
+  }
+})
+
+const system = createSystem(defaultConfig, themeConfig)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>,
