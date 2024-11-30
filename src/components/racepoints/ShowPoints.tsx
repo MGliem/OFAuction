eb60b32a6f370df37f82dbf0d406f08b22332d37
MGliem <maximilian.gliem@gmail.com>
2024-11-30 18:06:05 +0100

    feat: animations on points

--- src/components/racepoints/ShowPoints.tsx
+++ src/components/racepoints/ShowPoints.tsx
@@ -1,13 +1,19 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { motion } from "motion/react";
 
-function ShowPoints({
-  points
-}: {
-    points: number;
-}) {
+function ShowPoints({ points }: { points: number }) {
   return (
-    <Flex direction={"column-reverse"} width={"100%"} color={"#cecece"} flexGrow={1}>
-      <Text textAlign={"center"} fontSize={30}  paddingInline={15}>{points}</Text>
+    <Flex direction={"column-reverse"} width={"100%"} flexGrow={1}>
+      <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} key={points}>
+        <Text
+          textAlign={"center"}
+          fontSize={30}
+          paddingInline={15}
+          color={"#ebe53c"}
+        >
+          {points}
+        </Text>
+      </motion.div>
     </Flex>
   );
 }
